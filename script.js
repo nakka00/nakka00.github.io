849c2fc92339e9d370df337cb57ab9415871d3d5
nakka00 <48402187+nakka00@users.noreply.github.com>
2019-03-11 17:09:57 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+$(funtion() {
+
+  // アコーディオン
+  $('.faq-list-item').click(function() {
+    if ($(this).find('.answer').hasClass('answer-active')) {
+      $(this).find('.answer').removeClass('answer-active');
+      $(this).find('.answer').slideUp();
+      $('span').html("+");
+    } else {
+      $(this).find('.answer').addClass('answer-active');
+      $(this).find('.answer').slideDown();
+      $('span').html("-");
+    }
+  });
+  
+});
